387b20e75bce379f51d0d203145d11e3082bf06c
Andinh123 <45899032+Andinh123@users.noreply.github.com>
2023-10-27 19:54:37 +0700

    Version 1.0.0 Release

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+console.clear();
 const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
 const archiver = require('archiver');
 const express = require('express');
 const https = require('https');
+const fileDialog = require("popups-file-dialog");
 const app = express();
 let keepOpenState = true;
 function updateApp() {
     console.log("Updating app");
-    process.exit();
+    https.get("https://raw.githubusercontent.com/Andinh123/Addon-Compiler/main/instruction.json", (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+        response.on('end', () => {
+            try {
+                const updateData = JSON.parse(data);
+                console.log(updateData);
+                updateFiles(updateData);
+            } catch (error) {
+                console.error(`Error parsing JSON: ${error.message}`);
+                process.exit();
+            }
+        });
+    }).on('error', (error) => {
+        console.error(`Error fetching data: ${error.message}`);
+        process.exit();
+    });
 };
 function addonProject(array1, array2) {
     const exclusiveTo1 = array1.filter(element => !array2.includes(element)).map(element => element + " RP");
@@ -46,11 +66,21 @@ app.use(express.static('public'));
 app.get('/data', (req, res) => {
     res.send(addonProject(RPprojects, BPprojects));
 });
-app.post('/action/:parameter', (req, res) => {
+app.post('/action/:parameter/:choice', (req, res) => {
     const parameter = req.params.parameter.replace(/ (BP|RP|Addon)$/, '');
-    console.log(parameter);
-    createAddon(parameter);
-    res.send(`Action ${parameter} received`);
+    const choice = req.params.choice;
+    console.log(`/action/${parameter}/${choice}`);
+    if (choice === "choose") { 
+        (async () => {
+            const result = await fileDialog.openDirectory({
+                title: "Select a directory to save the Addon",
+            })
+            console.log(result);
+            createAddon(parameter, result);
+        })();
+    } else {
+        createAddon(parameter, path.join(process.env.USERPROFILE, 'Desktop'));
+    }
 });
   
 app.listen('80', () => {});
@@ -77,24 +107,23 @@ app.listen('80', () => {});
     });
 })();
 
-function createAddon(projectName) {
+function createAddon(projectName, outputDir) {
     const basePath = path.join(process.env.USERPROFILE, 'AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang');
-    const addonZipPath = path.join(process.env.USERPROFILE, 'Desktop', `${projectName} Addon.mcaddon`);
+    const addonZipPath = path.join(outputDir, `${projectName} Addon.mcaddon`);
     let BPpath, RPpath;
     if (fs.existsSync(path.join(basePath, 'development_behavior_packs', projectName + ' BP'))) {
-        BPpath = path.join(basePath, 'development_behavior_packs', projectName + ' BP')
+        BPpath = path.join(basePath, 'development_behavior_packs', projectName + ' BP');
     } else {
-        BPpath = path.join(basePath, 'development_behavior_packs', projectName)
-    };
-
+        BPpath = path.join(basePath, 'development_behavior_packs', projectName);
+    }
     if (fs.existsSync(path.join(basePath, 'development_resource_packs', projectName + ' RP'))) {
-        RPpath = path.join(basePath, 'development_resource_packs', projectName + ' RP')
+        RPpath = path.join(basePath, 'development_resource_packs', projectName + ' RP');
     } else {
-        RPpath = path.join(basePath, 'development_resource_packs', projectName)
+        RPpath = path.join(basePath, 'development_resource_packs', projectName);
     }
     const inputFolders = [
-        { name: projectName + ' BP', path: BPpath},
-        { name: projectName + ' RP', path: RPpath}
+        { name: projectName + ' BP', path: BPpath },
+        { name: projectName + ' RP', path: RPpath }
     ];
     const output = fs.createWriteStream(addonZipPath);
     const archive = archiver('zip', { zlib: { level: 9 } });
@@ -106,4 +135,39 @@ function createAddon(projectName) {
         archive.directory(folderInfo.path, folderInfo.name);
     });
     archive.finalize();
+}
+function updateFiles(updateData) {
+    const downloadPromises = [];
+    updateData.forEach(([filename, url]) => {
+    const promise = new Promise((resolve, reject) => {
+        https.get(url, (response) => {
+        if (response.statusCode === 200) {
+            const fileStream = fs.createWriteStream(path.join(__dirname, filename));
+            response.pipe(fileStream);
+
+            fileStream.on('finish', () => {
+            fileStream.close();
+            console.log(`Updated: ${filename}`);
+            resolve();
+            });
+        } else {
+            console.error(`Failed to download: ${filename} (HTTP ${response.statusCode})`);
+            reject(`Failed to download: ${filename}`);
+        }
+        }).on('error', (err) => {
+        console.error(`Error downloading ${filename}: ${err.message}`);
+        reject(`Error downloading ${filename}`);
+        });
+    });
+    downloadPromises.push(promise);
+    });
+    Promise.all(downloadPromises)
+    .then(() => {
+        console.log('Update done');
+        process.exit();
+    })
+    .catch((error) => {
+        console.error('Update failed:', error);
+        process.exit(1);
+    });
 }
